0cea79829f4586c99f8aadeee7a94c4351ca5abb
Matt Miller <matt123miller@hotmail.co.uk>
2018-06-14 22:47:52 +0100

    Made some shitty squares and you can drag some via Draggable.cs

---
+++ keith-sim/Assets/Scripts/DragHandler.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragHandler : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler {
+
+    public Vector2 startPos;
+    public Vector2 direction;
+
+    public Draggable draggedObject;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 mousePos = Input.mousePosition;
+
+            Ray ray = Camera.main.ScreenPointToRay(mousePos);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                // the object identified by hit.transform was clicked
+                // do whatever you want
+                print(hit.transform);
+
+                if (!draggedObject)
+                {
+                    draggedObject = hit.transform.GetComponent<Draggable>();
+
+                    if (!draggedObject)
+                    {
+                        return;
+                    }
+                    draggedObject.BeginDrag();
+                }
+
+                if (draggedObject)
+                {
+                    draggedObject.DragTo(mousePos);
+                }
+            }
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            if (!draggedObject) return;
+            draggedObject.EndDrag();
+        }
+    }
+
+    void IBeginDragHandler.OnBeginDrag(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    void IDragHandler.OnDrag(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    void IEndDragHandler.OnEndDrag(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+}
---
+++ keith-sim/Assets/Scripts/Draggable.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class Draggable : MonoBehaviour {
+
+        private Vector3 screenPoint;
+        private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnMouseDown()
+    {
+        BeginDrag();
+    }
+
+    void OnMouseDrag()
+    {
+        MoveFood();
+    }
+
+    void OnMouseUp()
+    {
+        // If your mouse hovers over the GameObject with the script attached, output this message
+        EndDrag(); 
+
+        // Ray cast, preferably avoiding the layer the food is on.
+
+        // Is it now on the bbq?
+    }
+
+    public void BeginDrag()
+    {
+        print("drag begins");
+        offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+
+    }
+
+    public void MoveFood()
+    {
+        Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+        Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
+        transform.position = curPosition;
+    }
+
+    public void DragTo(Vector2 pos)
+    {
+        transform.position = pos;
+    }
+
+    public void EndDrag()
+    {
+        print("Drag ended!");
+    }
+}
