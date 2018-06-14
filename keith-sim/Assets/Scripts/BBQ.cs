b82d8478559391335a60232463ed5a067d3e6bf8
Matt Miller <matt123miller@hotmail.co.uk>
2018-06-14 23:00:30 +0100

    Made BBQ.cs and Food.cs, BBQ accepts Food.
    BBQ will slowly cook it's list of food.

---
+++ keith-sim/Assets/Scripts/BBQ.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BBQ : MonoBehaviour {
+
+    public List<Food> food = new List<Food>();
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void AcceptFood(Food _food)
+    {
+        food.Add(_food);
+    }
+}
