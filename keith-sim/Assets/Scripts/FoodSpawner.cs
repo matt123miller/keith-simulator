2d16ec612af062f7a19f3e532b00bfd3030bd915
Matt Miller <matt123miller@hotmail.co.uk>
2018-06-16 13:20:40 +0100

    Intend to spawn food off screen anywhere between position.y and 0

---
+++ keith-sim/Assets/Scripts/FoodSpawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodSpawner : MonoBehaviour {
+
+// Spawn food anywhere between the bbq and the bottom of the screen.
+// rotate sausages a little
+
+	public Transform bbqRoot;
+	public float[] spawnRates;
+	public int spawnIndex = 0;
+	public float spawnCounter;
+
+
+
+	// Use this for initialization
+	void Awake () {
+		float x = bbqRoot.position.x - (bbqRoot.localScale.x);
+		float y = bbqRoot.position.y - (bbqRoot.localScale.y / 2);
+		
+		// Position is set to the bottom left of the bbq, slightly off screen
+		transform.position = new Vector2(x, y);
+
+		// Now you can spawn food off screen anywhere between position.y and 0
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
