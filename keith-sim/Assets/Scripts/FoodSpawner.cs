3d8a66fdcdfb529944112adf48ec615e286801a5
Matt Miller <matt123miller@hotmail.co.uk>
2018-06-16 13:43:18 +0100

    Setup some variable spawn rate stuff. Should probably use Random though

--- keith-sim/Assets/Scripts/FoodSpawner.cs
+++ keith-sim/Assets/Scripts/FoodSpawner.cs
@@ -10,7 +10,9 @@ public class FoodSpawner : MonoBehaviour {
 	public Transform bbqRoot;
 	public float[] spawnRates;
 	public int spawnIndex = 0;
+	public float currentSpawnThreshold;
 	public float spawnCounter;
+
 
 
 
@@ -21,12 +23,26 @@ public class FoodSpawner : MonoBehaviour {
 		
 		// Position is set to the bottom left of the bbq, slightly off screen
 		transform.position = new Vector2(x, y);
-
 		// Now you can spawn food off screen anywhere between position.y and 0
+
+		currentSpawnThreshold = spawnRates[spawnIndex];
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+		spawnCounter += Time.deltaTime;
+
+		if(spawnCounter > currentSpawnThreshold)
+		{
+			print("new food! " + currentSpawnThreshold);
+			spawnIndex++;
+			spawnCounter = 0;
+			if(spawnIndex >= spawnRates.Length)
+			{
+				spawnIndex = 0;
+			}
+			currentSpawnThreshold = spawnRates[spawnIndex];
+		}
 	}
 }
